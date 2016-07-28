9e3c16eb9c67c70e2b45f49686d8422f50f5b97a
rahulyadav20111995 <rahulyadav574@gmail.com>
2016-07-28 10:09:42 +0530

    Landing Page Template

--- utils/config.js
+++ utils/config.js
@@ -21,6 +21,7 @@ module.exports = function(app){
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(cookieParser());
-    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.static(path.join(__dirname, '../public')));
+    
     
 };
