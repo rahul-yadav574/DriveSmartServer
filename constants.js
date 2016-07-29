987447cd8290d5c6151504fcbace3ac5cd9b18da
Rahul Yadav <rahulyadav574@gmail.com>
2016-07-29 13:34:21 +0530

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Brekkishhh on 27-07-2016.
+ */
+
+
+module.exports = {
+    
+    api_key:'AIzaSyDRmmTDEhCywTDsnl5gXsuH1RFCcF89sXo',
+    google_maps_api_url:'https://maps.googleapis.com/maps/api/place/nearbysearch/json?',
+    sms_api_user:'rahulyadav574@gmail.com',
+    sms_api_hash:'58f535ac27a5a883e4b6fa44277893b299dfa9b6',
+    sms_api_sender:'TXTLCL'
+    
+    
+};
