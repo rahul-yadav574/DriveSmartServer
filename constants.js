da1fdf7819fc52b9241ee48fba032756b9e8917e
Rahul Yadav <rahulyadav574@gmail.com>
2016-07-30 18:06:35 +0530

    Update constants.js

--- constants.js
+++ constants.js
@@ -7,8 +7,8 @@ module.exports = {
     
     api_key:'AIzaSyDRmmTDEhCywTDsnl5gXsuH1RFCcF89sXo',
     google_maps_api_url:'https://maps.googleapis.com/maps/api/place/nearbysearch/json?',
-    sms_api_user:'rahulyadav574@gmail.com',
-    sms_api_hash:'58f535ac27a5a883e4b6fa44277893b299dfa9b6',
+    sms_api_user:'yadavgouravsingh1999@gmail.com',
+    sms_api_hash:'0808de856e132788e1bf20023d8bb56dc5fd1626',
     sms_api_sender:'TXTLCL'
     
     
