aaa398908f73a66693f97d32f401002562e53342
rahulyadav20111995 <rahulyadav574@gmail.com>
2016-08-27 18:15:52 +0530

    image compresses added

--- routes/guidebot.js
+++ routes/guidebot.js
@@ -57,7 +57,7 @@ router.post('/',function (req,res,next) {
             request({
                     url:api_url,
                     method:'POST',
-                    form:{apikey:'439a27da-a17c-410c-9201-b8e12f6ddade',file:bitmap}
+                    form:{apikey:'439a27da-a17c-410c-9201-b8e12f6ddade',file:image}
 
                 },
                 function (error,response,body) {
