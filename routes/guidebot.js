09cf1651b60636a7dcec8f52665910eae3759126
rahulyadav20111995 <rahulyadav574@gmail.com>
2016-08-27 18:32:27 +0530

    image compresses added

--- routes/guidebot.js
+++ routes/guidebot.js
@@ -48,8 +48,7 @@ router.post('/',function (req,res,next) {
 
     jimp.read(bitmap,function (err,image) {
 
-        image.resize(128, 128)            // resize
-            .quality(40);
+       
         if (err){
             res.send({status:false,data:'Bot is not able to read the image'});
         }
