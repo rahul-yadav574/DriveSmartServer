0fe06c835dce2d0ebc598d6f68f0c8a65e4004b3
rahulyadav20111995 <rahulyadav574@gmail.com>
2016-08-27 17:36:44 +0530

    Issue fixed

--- routes/guidebot.js
+++ routes/guidebot.js
@@ -5,7 +5,6 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 var request = require('request');
-var play = require('../public/images/play-button.png');
 
 
 router.get('/',function (req,res,next) {
